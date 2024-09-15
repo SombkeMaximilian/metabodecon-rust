b30988a7cad9be9ecac66520c22e4a92b6e0717b
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-09-15 17:47:24 +0200

    Add Lorentzian struct initializer that takes the function parameters as arguments

--- metabodecon/src/lorentzian.rs
+++ metabodecon/src/lorentzian.rs
@@ -10,4 +10,13 @@ impl Lorentzian {
     pub fn new() -> Self {
         Self { sfhw: 0., hw2: 0., maxp: 0. }
     }
+
+    pub fn from_param(scale_factor: f64, half_width: f64, maximum_position: f64) -> Self {
+        Self {
+            sfhw: scale_factor * half_width,
+            hw2: half_width.powi(2),
+            maxp: maximum_position
+        }
+    }
+
 }
