8810ba580d1c402f569f636355f0174370362430
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-09-15 17:48:28 +0200

    Add accessors and mutators of Lorentzian struct

--- metabodecon/src/lorentzian.rs
+++ metabodecon/src/lorentzian.rs
@@ -19,6 +19,41 @@ impl Lorentzian {
         }
     }
 
+    pub fn scale_factor(&self) -> f64 {
+        self.sfhw / self.hw2.sqrt()
+    }
+
+    pub fn half_width(&self) -> f64 {
+        self.hw2.sqrt()
+    }
+
+    pub fn maximum_position(&self) -> f64 {
+        self.maxp
+    }
+
+    pub fn parameters(&self) -> (f64, f64, f64) {
+        (self.scale_factor(), self.half_width(), self.maximum_position())
+    }
+
+    pub fn set_scale_factor(&mut self, scale_factor: f64) {
+        self.sfhw = scale_factor * self.hw2.sqrt();
+    }
+
+    pub fn set_half_width(&mut self, half_width: f64) {
+        self.sfhw = self.sfhw / self.hw2.sqrt() * half_width.sqrt();
+        self.hw2 = half_width.powi(2);
+    }
+
+    pub fn set_maximum_position(&mut self, maximum_position: f64) {
+        self.maxp = maximum_position;
+    }
+
+    pub fn set_parameters(&mut self, scale_factor: f64, half_width: f64, maximum_position: f64) {
+        self.set_scale_factor(scale_factor);
+        self.set_half_width(half_width);
+        self.set_maximum_position(maximum_position);
+    }
+
     pub fn evaluate(&self, x: f64) -> f64 {
         self.sfhw / (self.hw2 + (x - self.maxp).powi(2))
     }
