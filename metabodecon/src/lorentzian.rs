b580d20c45cb9da8f4df67ac9c65ca0c25916325
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-09-15 17:46:37 +0200

    Change storage form of Lorentzian parameters to remove some computational overhead of evaluation

--- metabodecon/src/lorentzian.rs
+++ metabodecon/src/lorentzian.rs
@@ -1,17 +1,13 @@
 // file: src/lorentzian.rs
 
 pub struct Lorentzian {
-    scaling_factor: f64,
-    half_width: f64,
-    maximum_position: f64
+    sfhw: f64, // A * lambda
+    hw2: f64,  // lambda^2
+    maxp: f64  // x_0
 }
 
 impl Lorentzian {
     pub fn new() -> Self {
-        Self {
-            scaling_factor: 0.,
-            half_width: 0.,
-            maximum_position: 0.
-        }
+        Self { sfhw: 0., hw2: 0., maxp: 0. }
     }
 }
