0f0f741b86938aefc66bdc19a9fcbfdc12095d73
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-09-17 16:09:12 +0200

    Add SumCacheMA unit tests

--- metabodecon/src/smoothing/sum_cache_ma_sm.rs
+++ metabodecon/src/smoothing/sum_cache_ma_sm.rs
@@ -29,3 +29,28 @@ where Type: Copy + FromPrimitive + AddAssign + SubAssign + Div<Output = Type> + 
         self.sum * self.div
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn new() {
+        let sum_cache_ma : SumCacheMA<f32, 3> = SumCacheMA::new(0.0);
+        assert_eq!(sum_cache_ma.compute_average(), 0.0);
+    }
+
+    #[test]
+    fn add_value() {
+        let mut sum_cache_ma : SumCacheMA<f32, 3> = SumCacheMA::new(0.0);
+        sum_cache_ma.add_value(1.0);
+        assert_eq!(sum_cache_ma.compute_average(), 1.0/3.0);
+        sum_cache_ma.add_value(2.0);
+        assert_eq!(sum_cache_ma.compute_average(), 3.0/3.0);
+        sum_cache_ma.add_value(3.0);
+        assert_eq!(sum_cache_ma.compute_average(), 6.0/3.0);
+        sum_cache_ma.add_value(4.0);
+        assert_eq!(sum_cache_ma.compute_average(), 9.0/3.0);
+        sum_cache_ma.add_value(5.0);
+        assert_eq!(sum_cache_ma.compute_average(), 12.0/3.0);
+    }
+}
