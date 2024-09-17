10b008fde0c4fe4ed17c37553c07becf357e656e
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-09-17 17:54:28 +0200

    Update public interface of smoothing submodule

--- metabodecon/src/smoothing/mod.rs
+++ metabodecon/src/smoothing/mod.rs
@@ -1,2 +1,7 @@
-pub mod circular_buffer;
-pub mod ma_sum_cache;
+mod circular_buffer;
+mod ma;
+mod ma_sum_cache;
+
+pub use ma::MA;
+pub use ma_sum_cache::SumCacheMA;
+pub use circular_buffer::CircularBuffer;
