e1a8c5fc91b3b7eac82ec7df532db25304d75930
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-09-18 20:47:19 +0200

    Fix error in SumCacheMA average computation
    - Dividing by the window size works unless the buffer is not full
    - Store the denominator (div) as one divided by the number of elements in the buffer and update it whenever that number changes

--- metabodecon/src/smoothing/ma_sum_cache.rs
+++ metabodecon/src/smoothing/ma_sum_cache.rs
@@ -5,8 +5,10 @@ use std::ops::{AddAssign, SubAssign, Div, Mul};
 
 pub struct SumCacheMA<Type, const WINDOW_SIZE: usize> {
     buffer: CircularBuffer<Type, WINDOW_SIZE>,
+    num: usize,
     sum: Type,
-    div: Type
+    div: Type,
+    one: Type
 }
 
 impl<Type, const WINDOW_SIZE: usize> MovingAverage<Type, WINDOW_SIZE>
@@ -19,11 +21,16 @@ where
         self.sum += value;
         if let Some(popped_value) = self.buffer.next(value) {
             self.sum -= popped_value;
+        } else {
+            self.num += 1;
+            self.div = self.one / Type::from_usize(self.num).unwrap();
         }
     }
 
     fn pop_last(&mut self) -> Option<Type> {
         if let Some(popped_value) = self.buffer.pop() {
+            self.num -= 1;
+            self.div = self.one / Type::from_usize(self.num).unwrap();
             self.sum -= popped_value;
             Some(popped_value)
         } else {
@@ -48,8 +55,10 @@ where
     pub fn new() -> Self {
         Self {
             buffer: CircularBuffer::new(Type::zero()),
+            num: 0,
             sum: Type::zero(),
-            div: Type::from_u8(1).unwrap() / Type::from_usize(WINDOW_SIZE).unwrap()
+            div: Type::from_u8(1).unwrap(),
+            one: Type::from_u8(1).unwrap()
         }
     }
 }
@@ -67,9 +76,9 @@ mod tests {
     fn add_value() {
         let mut sum_cache_ma : SumCacheMA<f32, 3> = SumCacheMA::new();
         sum_cache_ma.add_value(1.0);
-        assert_eq!(sum_cache_ma.compute_average(), 1.0/3.0);
+        assert_eq!(sum_cache_ma.compute_average(), 1.0/1.0);
         sum_cache_ma.add_value(2.0);
-        assert_eq!(sum_cache_ma.compute_average(), 3.0/3.0);
+        assert_eq!(sum_cache_ma.compute_average(), 3.0/2.0);
         sum_cache_ma.add_value(3.0);
         assert_eq!(sum_cache_ma.compute_average(), 6.0/3.0);
         sum_cache_ma.add_value(4.0);
