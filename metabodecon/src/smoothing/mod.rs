70a6822eb5ba96faab737d892154c8ee298fcd09
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-09-18 02:39:14 +0200

    Add Smoother trait and MovingAverageSmoother to mod.rs

--- metabodecon/src/smoothing/mod.rs
+++ metabodecon/src/smoothing/mod.rs
@@ -1,7 +1,11 @@
 mod circular_buffer;
 mod ma;
 mod ma_sum_cache;
+mod sm;
+mod sm_ma;
 
 pub use ma::MovingAverage;
+pub use sm::Smoother;
+pub use sm_ma::MovingAverageSmoother;
 pub use ma_sum_cache::SumCacheMA;
 pub use circular_buffer::CircularBuffer;
