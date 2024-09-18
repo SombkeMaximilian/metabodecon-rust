154a9ca8939825d91eb874f7ae45a54cd35cac65
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-09-18 21:01:03 +0200

    Add SimpleMA to mod.rs

--- metabodecon/src/smoothing/mod.rs
+++ metabodecon/src/smoothing/mod.rs
@@ -1,5 +1,6 @@
 mod circular_buffer;
 mod ma;
+mod ma_simple;
 mod ma_sum_cache;
 mod sm;
 mod sm_ma;
@@ -7,5 +8,6 @@ mod sm_ma;
 pub use ma::MovingAverage;
 pub use sm::Smoother;
 pub use sm_ma::MovingAverageSmoother;
+pub use ma_simple::SimpleMA;
 pub use ma_sum_cache::SumCacheMA;
 pub use circular_buffer::CircularBuffer;
