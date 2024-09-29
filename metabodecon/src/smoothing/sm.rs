fdd4d3862c2969845cc2c07636d80fa32ff694b7
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-09-29 06:50:02 +0200

    Add SmoothingAlgo enum as the public API for the user to decide which smoothing algorithm to use

--- metabodecon/src/smoothing/sm.rs
+++ metabodecon/src/smoothing/sm.rs
@@ -1,3 +1,9 @@
+use crate::smoothing::sm_ma::MovingAverageAlgo;
+
+pub enum SmoothingAlgo {
+    MovingAverage { algo: MovingAverageAlgo, iterations: usize, window_size: usize }
+}
+
 pub trait Smoother<Type> {
     fn compute_smoothed(&mut self, values: &[Type]) -> Vec<Type>;
 }
