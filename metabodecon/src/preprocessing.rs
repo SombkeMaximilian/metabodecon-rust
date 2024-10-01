02d4e2e78a3157cfd5858690df6e2d7d1bc6fd83
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-10-01 02:08:24 +0200

    Add function body for preprocess_spectrum function

--- metabodecon/src/preprocessing.rs
+++ metabodecon/src/preprocessing.rs
@@ -1,8 +1,12 @@
 use crate::data::Spectrum;
 use crate::smoothing::{SmoothingAlgo};
 
-#[allow(dead_code, unused_variables)]
-pub fn preprocess_spectrum(spectrum: &mut Spectrum, smoothing_algo: SmoothingAlgo) {}
+pub fn preprocess_spectrum(spectrum: &mut Spectrum, smoothing_algo: SmoothingAlgo) {
+    let water_region_width = spectrum.water_region_width();
+    remove_water_signal(spectrum.intensities_mut(), water_region_width);
+    remove_negative_values(spectrum.intensities_mut());
+    smooth_intensities(spectrum.intensities_mut(), smoothing_algo);
+}
 
 #[allow(dead_code, unused_variables)]
 fn remove_water_signal(intensities: &mut [f64], water_region_width: f64) {}
