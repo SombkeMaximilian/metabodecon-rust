d706688518edd75ae59aa3f40d5c809c241518b4
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-10-01 02:30:59 +0200

    Add function body for remove_negative_values function

--- metabodecon/src/preprocessing.rs
+++ metabodecon/src/preprocessing.rs
@@ -11,8 +11,13 @@ pub fn preprocess_spectrum(spectrum: &mut Spectrum, smoothing_algo: SmoothingAlg
 #[allow(dead_code, unused_variables)]
 fn remove_water_signal(intensities: &mut [f64], water_region_width: f64) {}
 
-#[allow(dead_code, unused_variables)]
-fn remove_negative_values(intensities: &mut [f64]) {}
+fn remove_negative_values(intensities: &mut [f64]) {
+    for intensity in intensities.iter_mut() {
+        if *intensity < 0.0 {
+            *intensity = -*intensity;
+        }
+    }
+}
 
 fn smooth_intensities(intensities: &mut [f64], algorithm: SmoothingAlgo) {
     match algorithm {
