fa909cdbdcc17c7bfaf6ed7355b8d98fe1c0e0ec
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-10-04 13:28:20 +0200

    Add initial structure of peak detection algorithm

---
+++ metabodecon/src/peak_selection/detection.rs
@@ -0,0 +1,10 @@
+use crate::data::Peak;
+
+#[allow(dead_code, unused_variables)]
+pub fn detect_peaks(intensities: &[f64]) -> Vec<Peak> { Vec::<Peak>::new() }
+
+#[allow(dead_code, unused_variables)]
+fn second_derivative(intensities: &[f64]) -> Vec<f64> { vec![] }
+
+#[allow(dead_code, unused_variables)]
+fn find_maxima(second_derivative: &[f64]) -> Vec<usize> { vec![] }
