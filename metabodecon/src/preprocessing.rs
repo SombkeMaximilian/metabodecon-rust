4930415ad4925a5f2c550fd93cbbce2ee071f097
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-10-04 12:19:30 +0200

    Add function body for remove_water_signal function of preprocessing module

--- metabodecon/src/preprocessing.rs
+++ metabodecon/src/preprocessing.rs
@@ -2,14 +2,19 @@ use crate::data::Spectrum;
 use crate::smoothing::{Smoother, SmoothingAlgo, MovingAverageSmoother};
 
 pub fn preprocess_spectrum(spectrum: &mut Spectrum, smoothing_algo: SmoothingAlgo) {
-    let water_region_width = spectrum.water_width();
-    remove_water_signal(spectrum.intensities_mut(), water_region_width);
+    let water_boundaries_indices = spectrum.water_boundaries_indices();
+    remove_water_signal(spectrum.intensities_mut(), water_boundaries_indices);
     remove_negative_values(spectrum.intensities_mut());
     smooth_intensities(spectrum.intensities_mut(), smoothing_algo);
 }
 
-#[allow(dead_code, unused_variables)]
-fn remove_water_signal(intensities: &mut [f64], water_region_width: f64) {}
+fn remove_water_signal(intensities: &mut [f64], boundary_indices: (usize, usize)) {
+    let min_intensity = *intensities.iter()
+                                .min_by(|a, b| a.total_cmp(b))
+                                .unwrap_or(&0.);
+    let water_region = &mut intensities[boundary_indices.0..boundary_indices.1];
+    water_region.fill(min_intensity);
+}
 
 fn remove_negative_values(intensities: &mut [f64]) {
     for intensity in intensities.iter_mut() {
