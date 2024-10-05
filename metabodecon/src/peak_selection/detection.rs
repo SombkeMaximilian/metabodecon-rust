d5e9a6b6ae8d7597384e12e12593852e9c863fbf
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-10-05 06:01:39 +0200

    Add function body for second_derivative and find_peak_centers
     - Rename find_maxima to find_peak_centers
     - Add logic for computing the second derivative
     - Add logic for finding the positions in the intensities where the second derivative is negative and takes on a local minimum

--- metabodecon/src/peak_selection/detection.rs
+++ metabodecon/src/peak_selection/detection.rs
@@ -4,7 +4,17 @@ use crate::data::Peak;
 pub fn detect_peaks(intensities: &[f64]) -> Vec<Peak> { Vec::<Peak>::new() }
 
 #[allow(dead_code, unused_variables)]
-fn second_derivative(intensities: &[f64]) -> Vec<f64> { vec![] }
+fn second_derivative(intensities: &[f64]) -> Vec<f64> {
+    intensities.windows(3)
+        .map(|w| w[0] - 2. * w[1] + w[2])
+        .collect()
+}
 
 #[allow(dead_code, unused_variables)]
-fn find_maxima(second_derivative: &[f64]) -> Vec<usize> { vec![] }
+fn find_peak_centers(second_derivative: &[f64]) -> Vec<usize> {
+    second_derivative.windows(3)
+        .enumerate()
+        .filter(|(_, w)| w[1] < w[0] && w[1] < 0. && w[1] < w[2])
+        .map(|(i, _)| i + 2)
+        .collect()
+}
