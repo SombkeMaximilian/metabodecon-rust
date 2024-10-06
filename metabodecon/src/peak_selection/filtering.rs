6a63a07e2eb23e3ae989faf2176766f09737ce21
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-10-06 15:32:24 +0200

    Change function arguments to slices instead of vectors

--- metabodecon/src/peak_selection/filtering.rs
+++ metabodecon/src/peak_selection/filtering.rs
@@ -1,10 +1,10 @@
 use crate::data::Peak;
 
 #[allow(dead_code, unused_variables)]
-pub fn filter_peaks(peaks: Vec<Peak>, abs_second_derivative: Vec<f64>) {}
+pub fn filter_peaks(peaks: &[Peak], abs_second_derivative: &[f64]) {}
 
 #[allow(dead_code, unused_variables)]
-fn score_peaks(peaks: Vec<Peak>, abs_second_derivative: Vec<f64>) -> Vec<f64> {
+fn score_peaks(peaks: &[Peak], abs_second_derivative: &[f64]) -> Vec<f64> {
     peaks.iter()
         .map(|peak| {
             f64::min(abs_second_derivative[peak.left()-1..peak.center()].iter().sum(),
@@ -21,6 +21,6 @@ mod tests {
     fn test_score_peaks() {
         let peaks = vec![Peak::from_pos(1, 3, 4), Peak::from_pos(5, 6, 9)];
         let abs_second_derivative = vec![1., 2., 4., 2., 2., 5., 4., 3., 2.];
-        assert_eq!(score_peaks(peaks, abs_second_derivative), vec![6., 7.]);
+        assert_eq!(score_peaks(&peaks, &abs_second_derivative), vec![6., 7.]);
     }
 }
