8c4be286b44a4cd590f276f59685ceca68ab43e0
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-10-06 08:10:28 +0200

    Add basic structure of filtering.rs

---
+++ metabodecon/src/peak_selection/filtering.rs
@@ -0,0 +1,15 @@
+use crate::data::Peak;
+
+#[allow(dead_code, unused_variables)]
+pub fn filter_peaks(peaks: Vec<Peak>, abs_second_derivative: Vec<f64>) {}
+
+#[allow(dead_code, unused_variables)]
+fn score_peaks(peaks: &mut Vec<Peak>, abs_second_derivative: Vec<f64>) {}
+
+#[cfg(test)]
+mod tests {
+    //use super::*;
+
+    #[test]
+    fn test_score_peaks() {}
+}
