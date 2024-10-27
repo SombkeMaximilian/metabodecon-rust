2a4c670c4694c25a89bf15c576f156e64d1272d3
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-10-27 05:12:21 +0100

    Add skeleton of FitterAnalytical struct

--- metabodecon/src/fitting/fitter_analytical.rs
+++ metabodecon/src/fitting/fitter_analytical.rs
@@ -1,1 +1,16 @@
+use crate::data::{Lorentzian, Spectrum};
+use crate::fitting::Fitter;
 
+pub struct FitterAnalytical {}
+
+impl Fitter for FitterAnalytical {
+    fn fit_lorentzian(&self, _spectrum: &Spectrum) -> Vec<Lorentzian> {
+        unimplemented!()
+    }
+}
+
+impl FitterAnalytical {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
