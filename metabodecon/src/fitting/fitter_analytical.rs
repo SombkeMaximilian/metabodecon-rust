f36265edf7c803ae0c56c6c7434e96834f301528
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-11-02 10:45:01 +0100

    Change the approximation helpers to not be associated functions

--- metabodecon/src/fitting/fitter_analytical.rs
+++ metabodecon/src/fitting/fitter_analytical.rs
@@ -30,7 +30,7 @@ impl FitterAnalytical {
         self.iterations = iterations;
     }
 
-    pub fn maximum_position(&self, p: &PeakStencilData) -> f64 {
+    pub fn maximum_position(p: &PeakStencilData) -> f64 {
         let numerator = p.x_1().powi(2) * p.y_1() * (p.y_2() - p.y_3())
             + p.x_2().powi(2) * p.y_2() * (p.y_3() - p.y_1())
             + p.x_3().powi(2) * p.y_3() * (p.y_1() - p.y_2());
@@ -40,7 +40,7 @@ impl FitterAnalytical {
         numerator / divisor
     }
 
-    pub fn half_width2(&self, p: &PeakStencilData, maxp: f64) -> f64 {
+    pub fn half_width2(p: &PeakStencilData, maxp: f64) -> f64 {
         let left = (p.y_1() * (p.x_1() - maxp).powi(2) - p.y_2() * (p.x_2() - maxp).powi(2))
             / (p.y_1() - p.y_2());
         let right = (p.y_2() * (p.x_2() - maxp).powi(2) - p.y_3() * (p.x_3() - maxp).powi(2))
@@ -48,7 +48,7 @@ impl FitterAnalytical {
         (left + right) / 2.
     }
 
-    pub fn scale_factor_half_width(&self, p: &PeakStencilData, maxp: f64, hw2: f64) -> f64 {
+    pub fn scale_factor_half_width(p: &PeakStencilData, maxp: f64, hw2: f64) -> f64 {
         p.x_2() * (hw2 + (p.x_2() - maxp).powi(2))
     }
 }
