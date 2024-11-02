ce9410ca509218d97326cc8c4063b1141fc00560
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-11-02 09:40:25 +0100

    Add `peaks` parameter to fit_lorentzian function in Fitter trait

--- metabodecon/src/fitting/fitter.rs
+++ metabodecon/src/fitting/fitter.rs
@@ -1,9 +1,9 @@
-use crate::data::{Lorentzian, Spectrum};
+use crate::data::{Lorentzian, Peak, Spectrum};
 
 pub enum FittingAlgo {
     Analytical { iterations: usize },
 }
 
 pub trait Fitter {
-    fn fit_lorentzian(&self, spectrum: &Spectrum) -> Vec<Lorentzian>;
+    fn fit_lorentzian(&self, spectrum: &Spectrum, peaks: &[Peak]) -> Vec<Lorentzian>;
 }
