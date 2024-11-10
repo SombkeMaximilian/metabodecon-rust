595e215f0b1e748f5332cff6f77236402dbbeaf0
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-11-10 18:23:35 +0100

    Remove Spectrum from Deconvolution output and add mean squared error

--- metabodecon/src/data/deconvolution.rs
+++ metabodecon/src/data/deconvolution.rs
@@ -1,24 +1,22 @@
-use crate::data::lorentzian::Lorentzian;
-use crate::data::spectrum::Spectrum;
+use crate::data::Lorentzian;
 
 pub struct Deconvolution {
     lorentzians: Vec<Lorentzian>,
-    spectrum: Spectrum,
+    mse: f64,
 }
 
 impl Deconvolution {
     pub fn new() -> Self {
         Self {
             lorentzians: Vec::new(),
-            spectrum: Spectrum::new(),
+            mse: 0.0,
         }
     }
 
     pub fn lorenztians(&self) -> &Vec<Lorentzian> {
         &self.lorentzians
     }
-
-    pub fn spectrum(&self) -> &Spectrum {
-        &self.spectrum
+    pub fn mse(&self) -> f64 {
+        self.mse
     }
 }
