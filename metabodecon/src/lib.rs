a1ad85495b51477cbb3cc69003d3afaac86b88e7
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-11-10 20:45:13 +0100

    Update public API of the crate

--- metabodecon/src/lib.rs
+++ metabodecon/src/lib.rs
@@ -1,6 +1,11 @@
-pub mod data;
-pub mod deconvoluter;
-pub mod fitting;
-pub mod peak_selection;
-pub mod preprocessing;
-pub mod smoothing;
+mod data;
+mod deconvoluter;
+mod fitting;
+mod peak_selection;
+mod preprocessing;
+mod smoothing;
+
+pub use data::{Deconvolution, Lorentzian, Spectrum};
+pub use deconvoluter::Deconvoluter;
+pub use fitting::{Fitter, FitterAnalytical, FittingAlgo};
+pub use smoothing::{MovingAverageAlgo, SmoothingAlgo};
