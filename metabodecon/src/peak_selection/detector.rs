b6395316534a0f230e689618b108e82466bc0f3a
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-11-23 04:42:33 +0100

    Add refactored Detector tests

--- metabodecon/src/peak_selection/detector.rs
+++ metabodecon/src/peak_selection/detector.rs
@@ -57,3 +57,83 @@ impl<'a> Detector<'a> {
             .map_or(second_derivative_left.len(), |i| i + 1)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn find_peak_centers() {
+        let second_derivative = vec![0., -2., 0.];
+        let detector = Detector::new(&second_derivative);
+        let peak_centers = detector.find_peak_centers();
+        assert_eq!(peak_centers, vec![2]);
+    }
+
+    #[test]
+    fn find_peak_borders() {
+        // indices are offset by 1, as second derivative is computed for central points only
+        let mut second_derivative = vec![0.5, -0.5, -1., 0., 0.5, 0.];
+        {
+            let detector = Detector::new(&second_derivative);
+            assert_eq!(
+                detector.find_peak_borders(&[3]),
+                vec![(2, 5)]
+            );
+        }
+        second_derivative = vec![0., 0.5, 0., -1., -0.5, 0.5];
+        {
+            let detector = Detector::new(&second_derivative);
+            assert_eq!(
+                detector.find_peak_borders(&[4]),
+                vec![(2, 5)]
+            );
+        }
+        second_derivative = vec![1., 1., 1., 1.5, 1.];
+        {
+            let detector = Detector::new(&second_derivative);
+            assert_eq!(
+                detector.find_peak_borders(&[3]),
+                vec![(0, 4)]
+            );
+        }
+        second_derivative = vec![1., 1.5, 1., 1., 1.];
+        {
+            let detector = Detector::new(&second_derivative);
+            assert_eq!(
+                detector.find_peak_borders(&[3]),
+                vec![(2, 6)]
+            );
+        }
+        second_derivative = vec![1., 1., 1., 1., 1.];
+        {
+            let detector = Detector::new(&second_derivative);
+            assert_eq!(
+                detector.find_peak_borders(&[3]),
+                vec![(0, 6)]
+            );
+        }
+    }
+
+    #[test]
+    fn find_right_border() {
+        let mut second_derivative = vec![0., -2., -1., -0.5, 0.5];
+        let detector = Detector::new(&second_derivative);
+        assert_eq!(Detector::find_right_border(&second_derivative[2..]), 1);
+        second_derivative = vec![0., -2., -1., 0., 0.5, 0.];
+        assert_eq!(Detector::find_right_border(&second_derivative[2..]), 2);
+        second_derivative = vec![1., 1., 1., 1., 1.];
+        assert_eq!(Detector::find_right_border(&second_derivative[2..]), 3);
+    }
+
+    #[test]
+    fn find_left_border() {
+        let mut second_derivative = vec![0.5, -0.5, -1., -2., 0.];
+        let detector = Detector::new(&second_derivative);
+        assert_eq!(Detector::find_left_border(&second_derivative[0..=2]), 1);
+        second_derivative = vec![0., 0.5, 0., -1., -2., 0.];
+        assert_eq!(Detector::find_left_border(&second_derivative[0..=3]), 2);
+        second_derivative = vec![1., 1., 1., 1., 1.];
+        assert_eq!(Detector::find_left_border(&second_derivative[0..=2]), 3);
+    }
+}
