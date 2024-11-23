6189148abae1f6d4b339c35c0933a6008def1fbf
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-11-23 05:43:17 +0100

    Refactor filter_peaks function to use the new scoring

--- metabodecon/src/peak_selection/filtering.rs
+++ metabodecon/src/peak_selection/filtering.rs
@@ -1,4 +1,5 @@
 use crate::data_structures::Peak;
+use crate::peak_selection::scorer::*;
 
 pub fn filter_peaks(
     mut peaks: Vec<Peak>,
@@ -6,23 +7,20 @@ pub fn filter_peaks(
     signal_boundaries: (usize, usize),
     threshold: f64,
 ) -> Vec<Peak> {
+    let scorer = ScorerMinimumSum::new(abs_second_derivative);
     let boundaries = peak_region_boundaries(&peaks, signal_boundaries);
-    let (mean, sd) = mean_sd_sfr_scores(&peaks, &abs_second_derivative, boundaries);
+    let left_sfr = &peaks[0..boundaries.0];
+    let right_sfr = &peaks[boundaries.1..];
+    let scores_sfr: Vec<f64> = left_sfr
+        .iter()
+        .chain(right_sfr.iter())
+        .map(|peak| scorer.score_peak(peak))
+        .collect();
+    let (mean, sd) = mean_sd_sfr_scores(scores_sfr);
     peaks
         .drain(boundaries.0..boundaries.1)
-        .filter(|peak| score_peak(peak, &abs_second_derivative) >= mean + threshold * sd)
+        .filter(|peak| scorer.score_peak(peak) >= mean + threshold * sd)
         .collect()
-}
-
-fn score_peak(peak: &Peak, abs_second_derivative: &[f64]) -> f64 {
-    f64::min(
-        abs_second_derivative[peak.left() - 1..peak.center()]
-            .iter()
-            .sum(),
-        abs_second_derivative[peak.center() - 1..peak.right()]
-            .iter()
-            .sum(),
-    )
 }
 
 fn peak_region_boundaries(peaks: &[Peak], signal_boundaries: (usize, usize)) -> (usize, usize) {
@@ -38,41 +36,20 @@ fn peak_region_boundaries(peaks: &[Peak], signal_boundaries: (usize, usize)) -> 
     (left, right)
 }
 
-fn mean_sd_sfr_scores(
-    peaks: &[Peak],
-    abs_second_derivative: &[f64],
-    peak_region_boundaries: (usize, usize),
-) -> (f64, f64) {
-    let left_sfr = &peaks[0..peak_region_boundaries.0];
-    let right_sfr = &peaks[peak_region_boundaries.1..];
-    let scores_sfr: Vec<f64> = left_sfr
-        .iter()
-        .chain(right_sfr.iter())
-        .map(|peak| score_peak(peak, &abs_second_derivative))
-        .collect();
-    let mean: f64 = scores_sfr.iter().sum::<f64>() / (left_sfr.len() + right_sfr.len()) as f64;
+fn mean_sd_sfr_scores(scores_sfr: Vec<f64>) -> (f64, f64) {
+    let mean: f64 = scores_sfr.iter().sum::<f64>() / scores_sfr.len() as f64;
     let variance: f64 = scores_sfr
         .iter()
         .map(|score| (score - mean).powi(2))
         .sum::<f64>()
-        / (left_sfr.len() + right_sfr.len()) as f64;
+        / scores_sfr.len() as f64;
     (mean, variance.sqrt())
 }
 
 #[cfg(test)]
 mod tests {
     use crate::data_structures::Peak;
-
-    #[test]
-    fn score_peak() {
-        let peaks = vec![Peak::new(1, 3, 4), Peak::new(5, 6, 9)];
-        let abs_second_derivative = vec![1., 2., 4., 2., 2., 5., 4., 3., 2.];
-        let scores: Vec<f64> = peaks
-            .iter()
-            .map(|peak| super::score_peak(peak, &abs_second_derivative))
-            .collect();
-        assert_eq!(scores, vec![6., 7.]);
-    }
+    use crate::peak_selection::scorer::*;
 
     #[test]
     fn peak_region_boundaries() {
@@ -94,9 +71,14 @@ mod tests {
             .map(|i| Peak::new(i - 1, i, i + 1))
             .collect();
         let abs_second_derivative = vec![1., 2., 4., 2., 2., 5., 4., 3., 2.];
+        let scorer = ScorerMinimumSum::new(&abs_second_derivative);
         let peak_region_boundaries = (1, 3);
-        let (mean, sd) =
-            super::mean_sd_sfr_scores(&peaks, &abs_second_derivative, peak_region_boundaries);
+        let scores_sfr: Vec<f64> = peaks[0..peak_region_boundaries.0]
+            .iter()
+            .chain(peaks[peak_region_boundaries.1..].iter())
+            .map(|peak| scorer.score_peak(peak))
+            .collect();
+        let (mean, sd) = super::mean_sd_sfr_scores(scores_sfr);
         assert_eq!(mean, 4.0);
         assert_eq!(sd, 1.0);
     }
