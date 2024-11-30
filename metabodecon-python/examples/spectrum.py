24272de2b10b9c810fb873a6d2d28c1b53825cc0
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-11-30 11:26:47 +0100

    Add example for bindings of Spectrum struct

---
+++ metabodecon-python/examples/spectrum.py
@@ -0,0 +1,21 @@
+import numpy as np
+import metabodecon_python as md
+
+
+def main():
+    spectrum = md.MdSpectrum(
+        chemical_shifts = np.array([1.0, 2.0, 3.0]),
+        intensities = np.array([10.0, 20.0, 30.0]),
+        signal_boundaries = (1.5, 2.5),
+        water_boundaries = (1.75, 2.25)
+    )
+    print(spectrum.chemical_shifts)
+    spectrum.intensities = np.array([100.0, 200.0, 300.0])
+    print(spectrum.intensities)
+    print(spectrum.intensities_raw)
+    print(spectrum.signal_boundaries)
+    print(spectrum.water_boundaries)
+
+
+if __name__ == "__main__":
+    main()
