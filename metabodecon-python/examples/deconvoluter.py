48de9029385b7fee7e0883087eb0ac6d3de9c574
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-12-04 16:37:47 +0100

    Update python examples for Deconvoluter with a better plot [skip ci]

--- metabodecon-python/examples/deconvoluter.py
+++ metabodecon-python/examples/deconvoluter.py
@@ -5,6 +5,8 @@ import time
 
 def main():
     blood = md.Spectrum.from_hdf5("../../metabodecon/data/blood.h5", "blood_01")
+    blood.signal_boundaries = (-2.208611, 11.807918)
+    blood.water_boundaries = (4.699535, 4.899771)
     deconvoluter = md.Deconvoluter(nfit = 10, sm_iter = 2, sm_ws = 5, delta = 6.4)
     t1 = time.time()
     deconvolution = deconvoluter.deconvolute_spectrum(blood)
@@ -14,8 +16,22 @@ def main():
     deconvolution = deconvoluter.par_deconvolute_spectrum(blood)
     t2 = time.time()
     print(f"Parallel {(t2 - t1) * 1000:.3f}ms")
+
+    x = blood.chemical_shifts
+    y = deconvolution.par_superposition_vec(blood.chemical_shifts)
+    s = blood.signal_boundaries
+    w = blood.water_boundaries
     plt.figure(figsize = (12, 8), dpi = 300)
-    plt.plot(blood.chemical_shifts, deconvolution.par_superposition_vec(blood.chemical_shifts))
+    plt.plot(x, y, label = "Deconvoluted Spectrum")
+    plt.gca().invert_xaxis()
+    plt.axvline(x = s[0], color = "black", label = "Signal Boundaries")
+    plt.axvline(x = s[1], color = "black")
+    plt.axvspan(w[0], w[1], color = "cyan", alpha = 0.3, label = "Water Region")
+    plt.xlabel("Chemical Shifts", fontsize = 16)
+    plt.ylabel("Intensity", fontsize = 16)
+    plt.xticks(fontsize = 14)
+    plt.yticks(fontsize = 14)
+    plt.legend()
     plt.show()
 
 
