0e67eb15f27156d0aa10c8c33be7a5fb9950cc50
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-12-10 01:35:09 +0100

    Refactor read_spectrum function of Hdf5Reader struct and add read_spectra function
    - Move logic to read a single spectrum into an internal function that takes a File handle reference
    - Use internal function to implement the public facing functions

--- metabodecon/src/spectrum/hdf5_reader.rs
+++ metabodecon/src/spectrum/hdf5_reader.rs
@@ -21,6 +21,24 @@ impl<P: AsRef<Path>> Hdf5Reader<P> {
 
     pub fn read_spectrum(&self, dataset: &str) -> hdf5::Result<Spectrum> {
         let file = hdf5::File::open(self.path.as_ref())?;
+        Self::read_from_file(&file, dataset)
+    }
+
+    pub fn read_spectra(&self) -> hdf5::Result<Vec<Spectrum>> {
+        let file = hdf5::File::open(self.path.as_ref())?;
+        let datasets: Vec<String> = file
+                .member_names()?
+                .into_iter()
+                .collect();
+        let spectra = datasets
+            .into_iter()
+            .map(|dataset| Self::read_from_file(&file, &dataset))
+            .collect::<hdf5::Result<Vec<Spectrum>>>()?;
+
+        Ok(spectra)
+    }
+
+    fn read_from_file(file: &hdf5::File, dataset: &str) -> hdf5::Result<Spectrum> {
         let spectrum_group = file.group(dataset)?.group("spectrum")?;
         let data_group = spectrum_group.group("data")?;
         let meta_group = spectrum_group.group("meta")?;
