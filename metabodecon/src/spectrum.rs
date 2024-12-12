ddc76203cf35e157c3309714ed8c4b8684f30786
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-12-12 05:58:00 +0100

    Rustfmt pass

--- metabodecon/src/spectrum.rs
+++ metabodecon/src/spectrum.rs
@@ -2,8 +2,7 @@
 mod regex_capture_macros;
 mod bruker_reader;
 mod hdf5_reader;
-#[allow(dead_code)]
-mod jdx_reader;
+#[allow(dead_code)] mod jdx_reader;
 mod spectrum;
 
 pub use bruker_reader::BrukerReader;
