107e79ab5c52aa7bc0b7a54efa8a4d3758c32938
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-12-15 08:34:07 +0100

    Update metabodecon crate description

--- metabodecon/src/lib.rs
+++ metabodecon/src/lib.rs
@@ -1,7 +1,19 @@
 /*!
-An implementation of the algorithm proposed by [Koh et al.] for the deconvolution of 1D NMR spectra.
+A library for reading, deconvolution, alignment and postprocessing of 1D NMR spectra.
 
-[Koh et al.]: https://pubmed.ncbi.nlm.nih.gov/19804999
+Metabodecon is a collection of algorithms proposed in various scientific papers, as well as our
+own work, that have been implemented in Rust.
+
+* Tools for working with 1D NMR data, Lorentzian peak shapes, and deconvolution results
+* Functionality for reading data from the Bruker TopSpin, JCAMP-DX, and HDF5 formats
+* An implementation of the automated feature extraction algorithm for the deconvolution of 1D
+  NMR spectra described in [Koh et al. (2009)]
+* An implementation of the alignment algorithm described in [Vu et al. (2011)] and [Beirnaert et al.
+  (2018)]
+
+[Koh et al. (2009)]: https://doi.org/10.1016/j.jmr.2009.09.003.
+[Vu et al. (2011)]: https://doi.org/10.1186/1471-2105-12-405
+[Beirnaert et al. (2018)]: https://doi.org/10.1371/journal.pcbi.1006018
 */
 
 mod deconvolution;
