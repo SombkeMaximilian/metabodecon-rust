9d6297f8d8d096d2e6e45d30cfa644164f57c832
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-12-17 06:45:36 +0100

    Implement Display implementation for spectrum::Error

--- metabodecon/src/spectrum/error.rs
+++ metabodecon/src/spectrum/error.rs
@@ -64,8 +64,44 @@ impl From<hdf5::Error> for Error {
 
 impl core::fmt::Display for Error {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        use self::Kind::*;
         let description = match &self.kind {
-            _ => "unimplemented",
+            EmptyData {
+                chemical_shifts,
+                intensities,
+            } => format!(
+                "input data is empty: \
+                 chemical shifts has {} elements, \
+                 intensities has {} elements",
+                chemical_shifts, intensities
+            ),
+            LengthMismatchedData {
+                chemical_shifts,
+                intensities,
+            } => format!(
+                "input data lengths are mismatched: \
+                 chemical shifts has {} elements, \
+                 intensities has {} elements",
+                chemical_shifts, intensities
+            ),
+            MonotonicityMismatch {
+                chemical_shifts,
+                signal_boundaries,
+                water_boundaries,
+            } => format!(
+                "input data is not monotonic (intensities may be incorrect): \
+                 chemical shifts is {:?},\
+                 signal boundaries is {:?}, \
+                 water boundaries is {:?}",
+                chemical_shifts, signal_boundaries, water_boundaries
+            ),
+            NonUniformlySpacedData { positions } => format!(
+                "input data is not uniformly spaced: \
+                 step size at indices ({}, {}) differs from previous step",
+                positions.0, positions.1
+            ),
+            IoError { message } => format!("I/O error: {}", message),
+            Hdf5Error { message } => format!("HDF5 error: {}", message),
         };
         write!(f, "{description}")
     }
