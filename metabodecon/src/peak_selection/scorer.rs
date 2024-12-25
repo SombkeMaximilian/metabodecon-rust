f6c397b3e859ceea06ac7ff0599ce85ae8554972
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-12-25 11:51:52 +0100

    Add documentation for the peak scoring algorithm

--- metabodecon/src/peak_selection/scorer.rs
+++ metabodecon/src/peak_selection/scorer.rs
@@ -1,21 +1,34 @@
 use crate::peak_selection::peak::Peak;
 
+/// Scoring methods for the peaks.
 #[non_exhaustive]
 #[derive(Copy, Clone, Debug)]
 pub enum ScoringAlgo {
+    /// Minimum Sum of the absolute second derivative.
+    ///
+    /// The minimum sum scorer computes the sum of the absolute second
+    /// derivative values of the peak at the left and right sides of the
+    /// peak center. The smaller of the two sums is the score of the peak.
     MinimumSum,
 }
 
+/// Trait interface for the scoring algorithms.
 pub trait Scorer {
+    /// Scores the given peak.
     fn score_peak(&self, peak: &Peak) -> f64;
 }
 
+/// Peak scoring algorithm based on the sum of second derivative values.
 #[derive(Debug)]
 pub struct ScorerMinimumSum<'a> {
+    /// The absolute second derivative of the signal as a reference.
     abs_second_derivative: &'a [f64],
 }
 
 impl Scorer for ScorerMinimumSum<'_> {
+    /// Scores the given peak by computing the sum of the absolute second
+    /// derivative values at the left and right sides of the peak center.
+    /// Returns the smaller of the two sums as the score of the peak.
     fn score_peak(&self, peak: &Peak) -> f64 {
         f64::min(
             self.abs_second_derivative[peak.left() - 1..peak.center()]
@@ -29,6 +42,8 @@ impl Scorer for ScorerMinimumSum<'_> {
 }
 
 impl<'a> ScorerMinimumSum<'a> {
+    /// Creates a new `ScorerMinimumSum` with the given absolute second
+    /// derivative.
     pub fn new(abs_second_derivative: &'a [f64]) -> Self {
         ScorerMinimumSum {
             abs_second_derivative,
