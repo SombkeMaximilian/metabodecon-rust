b03b123c508351c796cb32ead159eca7213a7a5c
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-12-28 16:21:00 +0100

    Add documentation for fitting trait interface and its settings enum

--- metabodecon/src/fitting/fitter.rs
+++ metabodecon/src/fitting/fitter.rs
@@ -2,15 +2,24 @@ use crate::fitting::lorentzian::Lorentzian;
 use crate::peak_selection::Peak;
 use crate::spectrum::Spectrum;
 
+/// Fitting methods.
 #[non_exhaustive]
 #[derive(Copy, Clone, Debug)]
 pub enum FittingAlgo {
-    Analytical { iterations: usize },
+    /// Fitting by solving a system of linear equations analytically.
+    Analytical {
+        /// The number of iterations to refine the fit.
+        iterations: usize,
+    },
 }
 
+/// Trait interface for fitting algorithms.
 pub trait Fitter {
+    /// Fits Lorentzian functions to a spectrum using the given peaks.
     fn fit_lorentzian(&self, spectrum: &Spectrum, peaks: &[Peak]) -> Vec<Lorentzian>;
 
+    /// Fits Lorentzian functions to a spectrum using the given peaks in
+    /// parallel.
     #[cfg(feature = "parallel")]
     fn par_fit_lorentzian(&self, spectrum: &Spectrum, peaks: &[Peak]) -> Vec<Lorentzian>;
 }
