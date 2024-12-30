a2cb30eaa22b2453e82687d5b3e4137150286489
Sombke Maximilian <sombkemaximilian@gmail.com>
2024-12-30 15:48:27 +0100

    Add documentation for Deconvolution result

--- metabodecon/src/deconvolution/deconvolution.rs
+++ metabodecon/src/deconvolution/deconvolution.rs
@@ -2,16 +2,23 @@ use crate::fitting::{FittingAlgo, Lorentzian};
 use crate::peak_selection::SelectionAlgo;
 use crate::smoothing::SmoothingAlgo;
 
+/// Data structure representing the result of a deconvolution.
 #[derive(Clone, Debug)]
 pub struct Deconvolution {
+    /// The deconvoluted signals.
     lorentzians: Vec<Lorentzian>,
+    /// The smoothing parameters used.
     smoothing_algo: SmoothingAlgo,
+    /// The peak selection parameters used.
     selection_algo: SelectionAlgo,
+    /// The fitting parameters used.
     fitting_algo: FittingAlgo,
+    /// The mean squared error of the deconvolution.
     mse: f64,
 }
 
 impl Deconvolution {
+    /// Constructs a new `Deconvolution`.
     pub fn new(
         lorentzians: Vec<Lorentzian>,
         smoothing_algo: SmoothingAlgo,
@@ -28,22 +35,29 @@ impl Deconvolution {
         }
     }
 
+    /// Returns the deconvoluted signals as a slice of [`Lorentzian`].
+    ///
+    /// [`Lorentzian`]: Lorentzian
     pub fn lorentzians(&self) -> &[Lorentzian] {
         &self.lorentzians
     }
 
+    /// Returns the smoothing parameters used.
     pub fn smoothing_algo(&self) -> &SmoothingAlgo {
         &self.smoothing_algo
     }
 
+    /// Returns the peak selection parameters used.
     pub fn selection_algo(&self) -> &SelectionAlgo {
         &self.selection_algo
     }
 
+    /// Returns the fitting parameters used.
     pub fn fitting_algo(&self) -> &FittingAlgo {
         &self.fitting_algo
     }
 
+    /// Returns the mean squared error of the deconvolution.
     pub fn mse(&self) -> f64 {
         self.mse
     }
