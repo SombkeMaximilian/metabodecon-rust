d25f95c3c8e73f6e0f680d8d72cd859a597500b9
Sombke Maximilian <sombkemaximilian@gmail.com>
2025-01-09 10:15:09 +0100

    Add __version__ variable to python bindings

--- metabodecon-python/src/lib.rs
+++ metabodecon-python/src/lib.rs
@@ -6,6 +6,7 @@ use bindings::{Deconvoluter, Deconvolution, Lorentzian, Spectrum};
 
 #[pymodule]
 fn _metabodecon(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     m.add_class::<Deconvoluter>()?;
     m.add_class::<Deconvolution>()?;
     m.add_class::<Lorentzian>()?;
