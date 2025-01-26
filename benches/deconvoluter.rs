b47813ea68f9d346cde9a121153b27d46821f6cf
Sombke Maximilian <sombkemaximilian@gmail.com>
2025-01-26 22:52:31 +0100

    Simplify Deconvoluter benchmarking logic

--- benches/deconvoluter.rs
+++ benches/deconvoluter.rs
@@ -1,7 +1,7 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 use metabodecon::{deconvolution::*, spectrum::*};
 
-fn read_spectra() -> (Spectrum, Spectrum, Spectrum) {
+fn single_spectrum(c: &mut Criterion) {
     let reader = Hdf5Reader::new();
     let sim_spectrum = reader
         .read_spectrum("../data/hdf5/sim.h5", "sim_01")
@@ -9,50 +9,54 @@ fn read_spectra() -> (Spectrum, Spectrum, Spectrum) {
     let blood_spectrum = reader
         .read_spectrum("../data/hdf5/blood.h5", "blood_01")
         .unwrap();
-    let urine_spectrum = reader
-        .read_spectrum("../data/hdf5/urine.h5", "urine_1")
-        .unwrap();
-    (sim_spectrum, blood_spectrum, urine_spectrum)
-}
-
-fn deconvolute_spectrum(c: &mut Criterion) {
-    let (sim_spectrum, blood_spectrum, urine_spectrum) = read_spectra();
     let deconvoluter = Deconvoluter::default();
 
+    // Sequential
     c.bench_function("deconvolute_sim_spectrum", |b| {
         b.iter(|| deconvoluter.deconvolute_spectrum(&sim_spectrum))
     });
     c.bench_function("deconvolute_blood_spectrum", |b| {
         b.iter(|| deconvoluter.deconvolute_spectrum(&blood_spectrum))
     });
-    c.bench_function("deconvolute_urine_spectrum", |b| {
-        b.iter(|| deconvoluter.deconvolute_spectrum(&urine_spectrum))
-    });
-}
 
-fn par_deconvolute_spectrum(c: &mut Criterion) {
-    let (sim_spectrum, blood_spectrum, urine_spectrum) = read_spectra();
-    let deconvoluter = Deconvoluter::default();
-
+    // Parallel
     c.bench_function("parallel_deconvolute_sim_spectrum", |b| {
         b.iter(|| deconvoluter.par_deconvolute_spectrum(&sim_spectrum))
     });
     c.bench_function("parallel_deconvolute_blood_spectrum", |b| {
         b.iter(|| deconvoluter.par_deconvolute_spectrum(&blood_spectrum))
     });
-    c.bench_function("parallel_deconvolute_urine_spectrum", |b| {
-        b.iter(|| deconvoluter.par_deconvolute_spectrum(&urine_spectrum))
+}
+
+fn multiple_spectra(c: &mut Criterion) {
+    let reader = Hdf5Reader::new();
+    let sim_spectra = reader
+        .read_spectra("../data/hdf5/sim.h5")
+        .unwrap();
+    let blood_spectra = reader
+        .read_spectra("../data/hdf5/blood.h5")
+        .unwrap();
+    let deconvoluter = Deconvoluter::default();
+
+    // Only the parallel case is interesting here
+    c.bench_function("parallel_deconvolute_sim_spectra", |b| {
+        b.iter(|| deconvoluter.par_deconvolute_spectra(&sim_spectra))
+    });
+    c.bench_function("parallel_deconvolute_blood_spectra", |b| {
+        b.iter(|| deconvoluter.par_deconvolute_spectra(&blood_spectra))
     });
 }
 
-fn criterion_config() -> Criterion {
-    Criterion::default().measurement_time(std::time::Duration::from_secs(20))
+criterion_group! {
+    name = deconvolute_spectrum;
+    config = Criterion::default();
+    targets = single_spectrum
 }
 
 criterion_group! {
-    name = deconvoluter;
-    config = criterion_config();
-    targets = deconvolute_spectrum, par_deconvolute_spectrum
+    name = deconvolute_spectra;
+    config = Criterion::default();
+    targets = multiple_spectra
 }
 
-criterion_main!(deconvoluter);
+criterion_main!(deconvolute_spectrum, deconvolute_spectra);
