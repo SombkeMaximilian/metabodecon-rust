c203cc01980ffa754e6d60641dd4263fcc62f144
Sombke Maximilian <sombkemaximilian@gmail.com>
2025-02-11 18:32:39 +0100

    Rustfmt pass

--- metabodecon/tests/deconvoluter.rs
+++ metabodecon/tests/deconvoluter.rs
@@ -20,7 +20,9 @@ fn sim() {
     let path = "../data/bruker/sim/sim_01";
     let spectrum = Bruker::read_spectrum(path, 10, 10, (3.339007, 3.553942)).unwrap();
     let deconvoluter = Deconvoluter::default();
-    let deconvolution = deconvoluter.deconvolute_spectrum(&spectrum).unwrap();
+    let deconvolution = deconvoluter
+        .deconvolute_spectrum(&spectrum)
+        .unwrap();
     let filename = format!("{}_deconvolution.csv", "sim_01");
     store_deconvolution(deconvolution, filename.as_str());
 }
@@ -30,7 +32,9 @@ fn blood() {
     let path = "../data/bruker/blood/blood_01";
     let spectrum = Bruker::read_spectrum(path, 10, 10, (-2.2, 11.8)).unwrap();
     let deconvoluter = Deconvoluter::default();
-    let deconvolution = deconvoluter.deconvolute_spectrum(&spectrum).unwrap();
+    let deconvolution = deconvoluter
+        .deconvolute_spectrum(&spectrum)
+        .unwrap();
     let filename = format!("{}_deconvolution.csv", "blood_01");
     store_deconvolution(deconvolution, filename.as_str());
 }
@@ -41,7 +45,9 @@ fn par_sim() {
     let path = "../data/bruker/sim/sim_01";
     let spectrum = Bruker::read_spectrum(path, 10, 10, (3.339007, 3.553942)).unwrap();
     let deconvoluter = Deconvoluter::default();
-    let deconvolution = deconvoluter.par_deconvolute_spectrum(&spectrum).unwrap();
+    let deconvolution = deconvoluter
+        .par_deconvolute_spectrum(&spectrum)
+        .unwrap();
     let filename = format!("{}_par_deconvolution.csv", "sim_01");
     store_deconvolution(deconvolution, filename.as_str());
 }
@@ -52,7 +58,9 @@ fn par_blood() {
     let path = "../data/bruker/blood/blood_01";
     let spectrum = Bruker::read_spectrum(path, 10, 10, (-2.2, 11.8)).unwrap();
     let deconvoluter = Deconvoluter::default();
-    let deconvolution = deconvoluter.par_deconvolute_spectrum(&spectrum).unwrap();
+    let deconvolution = deconvoluter
+        .par_deconvolute_spectrum(&spectrum)
+        .unwrap();
     let filename = format!("{}_par_deconvolution.csv", "blood_01");
     store_deconvolution(deconvolution, filename.as_str());
 }
