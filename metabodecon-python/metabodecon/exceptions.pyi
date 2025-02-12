304fe390f92b2cb0dada5b35c2267def348e8eeb
Sombke Maximilian <sombkemaximilian@gmail.com>
2025-02-12 20:12:44 +0100

    Add SerializationError class to python package and replace ... with pass

--- metabodecon-python/metabodecon/exceptions.pyi
+++ metabodecon-python/metabodecon/exceptions.pyi
@@ -1,67 +1,89 @@
 class Error(Exception):
     """Base class for exceptions in this module."""
-    ...
+
+    pass
 
 class Unexpected(Error):
     """An exception raised for unexpected errors."""
-    ...
+
+    pass
+
+class SerializationError(Error):
+    """An exception raised for errors in serialization."""
+
+    pass
 
 class SpectrumError(Error):
     """An exception raised for errors in the Spectrum class."""
-    ...
+
+    pass
 
 class EmptyData(SpectrumError):
     """Input data is empty."""
-    ...
+
+    pass
 
 class DataLengthMismatch(SpectrumError):
     """Input data lengths do not match."""
-    ...
+
+    pass
 
 class NonUniformSpacing(SpectrumError):
     """Chemical shifts are not uniformly spaced."""
-    ...
+
+    pass
 
 class InvalidIntensities(SpectrumError):
     """Intensities contain invalid values."""
-    ...
+
+    pass
 
 class InvalidSignalBoundaries(SpectrumError):
     """Signal boundaries are invalid."""
-    ...
+
+    pass
 
 class MissingMetadata(SpectrumError):
     """Metadata is missing from NMR format-related file."""
-    ...
+
+    pass
 
 class DeconvolutionError(Error):
     """An exception raised for errors during the deconvolution process."""
-    ...
+
+    pass
 
 class InvalidSmoothingSettings(DeconvolutionError):
     """Smoothing settings are invalid."""
-    ...
+
+    pass
 
 class InvalidSelectionSettings(DeconvolutionError):
     """Peak selection settings are invalid."""
-    ...
+
+    pass
 
 class InvalidFittingSettings(DeconvolutionError):
     """Peak fitting settings are invalid."""
-    ...
+
+    pass
 
 class InvalidIgnoreRegion(DeconvolutionError):
     """Ignore region boundaries are invalid."""
-    ...
+
+    pass
 
 class NoPeaksDetected(DeconvolutionError):
     """No peaks were detected in the spectrum."""
-    ...
+
+    pass
 
 class EmptySignalRegion(DeconvolutionError):
     """Signal region contains no peaks."""
-    ...
+
+    pass
 
 class EmptySignalFreeRegion(DeconvolutionError):
     """Signal-free region contains no peaks."""
-    ...
+
+    pass
