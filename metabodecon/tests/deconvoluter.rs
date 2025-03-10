b1fb72ecfa986c5c1863b4c8eca8676cc725aea8
Sombke Maximilian <sombkemaximilian@gmail.com>
2025-03-10 14:01:04 +0100

    Update integration tests
    - Add utilities module
    - Use serialization to store the deconvolution result
    - Add parsing test which verifies that JCAMP-DX and Bruker parsing provide (roughly) the same result (precision is already diminished before anything is even parsed)

--- metabodecon/tests/deconvoluter.rs
+++ metabodecon/tests/deconvoluter.rs
@@ -1,66 +1,70 @@
 use metabodecon::{deconvolution::*, spectrum::*};
-use std::io::Write;
 
-#[cfg(test)]
-fn store_deconvolution(deconvolution: Deconvolution, filename: &str) {
-    let tmp_path = env!("CARGO_TARGET_TMPDIR");
-    let deconvolutions = format!("{}/test-deconvolutions", tmp_path);
-    std::fs::create_dir_all(deconvolutions).unwrap();
-    let filename = format!("{}/test-deconvolutions/{}", tmp_path, filename);
-    let mut file = std::fs::File::create(filename).unwrap();
-    writeln!(file, "sfhw,hw2,maxp").unwrap();
-    for lorentzian in deconvolution.lorentzians() {
-        let (sfhw, hw2, maxp) = lorentzian.parameters();
-        writeln!(file, "{},{},{}", sfhw, hw2, maxp).unwrap();
-    }
-}
+pub mod utilities;
+use utilities::{store_deconvolution, workspace_dir};
 
 #[test]
 fn sim() {
-    let path = "../data/bruker/sim/sim_01";
+    let path = workspace_dir()
+        .join("data")
+        .join("bruker")
+        .join("sim")
+        .join("sim_01");
     let spectrum = Bruker::read_spectrum(path, 10, 10, (3.35, 3.55)).unwrap();
     let deconvoluter = Deconvoluter::default();
     let deconvolution = deconvoluter
         .deconvolute_spectrum(&spectrum)
         .unwrap();
-    let filename = format!("{}_deconvolution.csv", "sim_01");
+    let filename = format!("{}_deconvolution.json", "sim_01");
     store_deconvolution(deconvolution, filename.as_str());
 }
 
 #[test]
 fn blood() {
-    let path = "../data/bruker/blood/blood_01";
+    let path = workspace_dir()
+        .join("data")
+        .join("bruker")
+        .join("blood")
+        .join("blood_01");
     let spectrum = Bruker::read_spectrum(path, 10, 10, (-2.2, 11.8)).unwrap();
     let deconvoluter = Deconvoluter::default();
     let deconvolution = deconvoluter
         .deconvolute_spectrum(&spectrum)
         .unwrap();
-    let filename = format!("{}_deconvolution.csv", "blood_01");
+    let filename = format!("{}_deconvolution.json", "blood_01");
     store_deconvolution(deconvolution, filename.as_str());
 }
 
 #[cfg(feature = "parallel")]
 #[test]
 fn par_sim() {
-    let path = "../data/bruker/sim/sim_01";
+    let path = workspace_dir()
+        .join("data")
+        .join("bruker")
+        .join("sim")
+        .join("sim_01");
     let spectrum = Bruker::read_spectrum(path, 10, 10, (3.35, 3.55)).unwrap();
     let deconvoluter = Deconvoluter::default();
     let deconvolution = deconvoluter
         .par_deconvolute_spectrum(&spectrum)
         .unwrap();
-    let filename = format!("{}_par_deconvolution.csv", "sim_01");
+    let filename = format!("{}_par_deconvolution.json", "sim_01");
     store_deconvolution(deconvolution, filename.as_str());
 }
 
 #[cfg(feature = "parallel")]
 #[test]
 fn par_blood() {
-    let path = "../data/bruker/blood/blood_01";
+    let path = workspace_dir()
+        .join("data")
+        .join("bruker")
+        .join("blood")
+        .join("blood_01");
     let spectrum = Bruker::read_spectrum(path, 10, 10, (-2.2, 11.8)).unwrap();
     let deconvoluter = Deconvoluter::default();
     let deconvolution = deconvoluter
         .par_deconvolute_spectrum(&spectrum)
         .unwrap();
-    let filename = format!("{}_par_deconvolution.csv", "blood_01");
+    let filename = format!("{}_par_deconvolution.json", "blood_01");
     store_deconvolution(deconvolution, filename.as_str());
 }
---
+++ metabodecon/tests/utilities.rs
@@ -0,0 +1,21 @@
+use metabodecon::deconvolution::Deconvolution;
+use std::path::PathBuf;
+
+// A lot of the conversions are already imprecise before the spectra are parsed,
+// so the required precision here is fairly lenient.
+pub const PRECISION: f64 = 1.0e-3;
+
+pub fn workspace_dir() -> PathBuf {
+    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    PathBuf::from(manifest_dir).join("..")
+}
+
+pub fn store_deconvolution(deconvolution: Deconvolution, filename: &str) {
+    let tmp_path = std::env::var("CARGO_TARGET_TMPDIR").unwrap();
+    let deconvolutions_dir = format!("{}/test-deconvolutions", tmp_path);
+    let filename = format!("{}/test-deconvolutions/{}", tmp_path, filename);
+    let serialized = serde_json::to_string_pretty(deconvolution.as_ref()).unwrap();
+    std::fs::create_dir_all(&deconvolutions_dir).unwrap();
+    std::fs::write(&filename, &serialized).unwrap();
+}
